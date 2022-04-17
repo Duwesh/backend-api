ada8c3e8ee116fb8f76c8b29502e7dd82fb55e88
Duwesh Kumar <dkduwesh07771@gmail.com>
2022-04-17 22:39:23 +0530

    fixed the error

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
-const middlewares = jsonServer.defaults({
-  static: "./build",
-});
-const PORT = process.env.PORT || 8080;
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const PORT = process.env.PORT || 3000;
+
 server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    "/*": "/$1",
-  })
-);
+server.use(router);
 server.use(router);
 server.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
